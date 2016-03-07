0549fd0a044b3ac655b4077811ec70a772e39b97
Devin Patel <cs.devin@gmail.com>
2016-03-07 13:03:48 -0500

    Angular Product Page withouth jQuery support - jQuery included to resolve bootstrap errors

--- public/javascripts/products_angular_productListapp.js
+++ public/javascripts/products_angular_productListapp.js
@@ -7,8 +7,24 @@ var controllers = {};
 		$http.get("/Products").then(function(response) 
 		{
 			$scope.ng_products = response.data;
-			$("#allProducts").show();
+			document.getElementById("allProducts").style.display = "block";
 		});
+		
+		$scope.addItem = function(item)
+		{
+			$http.post("/Products", item)
+				.success(function(response){
+					$scope.ng_products.push(response);
+				});
+		};
+		
+		$scope.removeItem = function(id)
+		{
+			$http.delete("/Products/" + id)
+				.success(function(){
+					$scope.ng_products.splice(id-1, 1);
+				});
+		}
 	};
 	
 	app.controller(controllers);
